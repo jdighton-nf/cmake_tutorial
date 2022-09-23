4936da4c9fc28b9f712f125ff0a411bcd02ac534
jdighton <jd.qz3f2@gmail.com>
2022-09-23 17:56:44 -0500

    adsf

---
+++ include/Blah.h
@@ -0,0 +1,10 @@
+#pragma once
+// no idea what this does
+#include <iostream> 
+
+class Blah {
+    public:
+        inline void boo(){
+            std::cout << "Boo!\n";
+        }
+}; 
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+#include "Blah.h"
+
+int main(){
+    std::cout << "Hello world\n";
+    Blah().boo(); 
+    return 0; 
+}
